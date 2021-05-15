c84c68a3be7e5c30a88fe94f5e1967eac3a5ad0b
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-15 14:39:15 -0700

    imported react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AppNav from "./components/Navbar";
 import Home from "./components/pages/Home";
 import Dashboard from "./components/pages/Dashboard";
