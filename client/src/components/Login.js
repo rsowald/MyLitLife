cca7101885228acc94c055148119907858db6062
Ingrid Lyublinsky <ingrid.lyub@gmail.com>
2021-05-18 08:35:31 -0700

    started backend oauth code with notes. Also added all the new npm packages I have added to this app in the TODO to keep track of it. I still am working through figuring out the Google API setup, and have started working on the React Login front end to assist in ensuring the backend is working.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,27 @@
 import React from "react";
 
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost:27017/gfg');
+var db=mongoose.connection;
+
+db.on('error', console.log.bind(console, "connection error"));
+
+db.once('open', function(callback){
+    console.log("connection succeeded");
+})
+
+var app=express()
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 function Login() {
     return (
         <></>
--- server.js
+++ server.js
@@ -1,7 +1,66 @@
 const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
 const path = require("path");
+const passport = require('passport');
+const cookieSession = require('cookie-session')
+require(''./utils/passportSetup');
+
+app.use(cors());
+
+//parse app - urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//parse app - JSON
+app.use(bodyParser.json());
+
+//Cookies - added a new npm package (cookie-sessions)
+app.use(cookieSession({
+  name: 'my-lit-life-session',
+  keys: ['key1', 'key2']
+}))
+
+//Middleware - login next steps & 401 page (not authorized)
+const userLoggedIn = (res,  req, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStauts(401);
+  }
+};
+
+
+
+
+//Starting session:
+
+//Auth
+app.use(passport.initialize());
+//Use sessions
+app.use(passport.session());
+
+//port
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//Google login authentication - scope
+app.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+//Successful login path - *** would like ti to say welcome one the first time loggining in....then welcome back based on the users login history. Will do a little more digging on how to write that out in the next itteration.
+app.get('/success', (req, res) => res.send(`Welcome back, ${req.user.email}!`));
+
+//Failed to login route
+app.get('/failed', (req, res) => res.send('Something went wrong. Please reattempt to login.'));
+
+//After logout
+app.get('/', (req, res) => res.send('You are no longer logged in.'));
+
+//Successful login route
+app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/success');
+  });
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -12,6 +71,12 @@ if (process.env.NODE_ENV === "production") {
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.get('/logout', (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect('/');
 });
 
 app.listen(PORT, function() {
