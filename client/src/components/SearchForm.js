a79f633d8b6874a60263cd8fdcdf7795da8f5dc8
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-18 21:28:22 -0700

    search form with no functionality

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -2,7 +2,27 @@ import React from "react";
 
 function SearchForm() {
     return (
-        <></>
+        <Container>
+            <Row style={{ border: "3px solid black", borderRadius: "5px", padding: "20px" }}>
+                <Col size="md-4">
+                    <Form style={{ padding: "10px" }}>
+                        <Form.Group controlId="searchForm.Input">
+                            <Form.Label>Search Terms</Form.Label>
+                            <Form.Control type="text" onChange={handleInputChange} placeholder="Keyword? Title? Author? Whatever." />
+                        </Form.Group>
+                        <Form.Group controlId="searchForm.Select">
+                            <Form.Label>Search By</Form.Label>
+                            <Form.Control as="select">
+                                <option value="title">Title</option>
+                                <option value="author">Author</option>
+                                <option value="genre">Genre</option>
+                            </Form.Control>
+                        </Form.Group>
+                        <Button variant="primary" className="float-right mb-2" onClick={handleSearch}>Search</Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
