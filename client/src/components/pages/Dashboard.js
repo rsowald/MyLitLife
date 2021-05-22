8e22371dfa38da120856208dbb56994a4d3db091
Victor <VictorCodrean@users.noreply.github.com>
2021-05-21 20:29:14 -0700

    link logout

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,11 +1,36 @@
 import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
 import app from "../authentication/Base";
 
 function Dashboard() {
     return (
         <>
-        <h2>Dashboard</h2>
-        <button onClick={() => app.auth().signOut()}>Sign Out</button>
+            <Container className="d-flex align-items-center justify-content-center"
+                fluid style={{
+                    backgroundImage: `url(./images/background.jpg)`,
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    width: '100vw',
+                    height: '100vh'
+                }}>
+                <Row>
+                    <Col md={3} className="text-center" style={{ backgroundColor: "gainsboro", borderRadius: "5px" }}>
+                        
+                       
+                            <h1>Hello World</h1>
+                       
+
+                    </Col>
+                    <Col md={6}>
+                        <h1>Hello World</h1>
+                    </Col>
+                    <Col md={3}>
+                        <h1>Hello World</h1>
+                    </Col>
+                </Row>
+
+            </Container >
         </>
     );
 };
