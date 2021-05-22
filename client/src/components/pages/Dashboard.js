a15f2e677cc33b4792fe9d3adca387f4c7573d0a
Victor <VictorCodrean@users.noreply.github.com>
2021-05-21 21:02:17 -0700

    sdfvd

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -4,6 +4,7 @@ import "./dashboard.css";
 function Dashboard() {
     return (
         <>
+
             <meta charSet="UTF-8" />
             <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
