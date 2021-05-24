dea08316a46a8a9b684c8ef45735182fcb0d60c7
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-23 19:55:09 -0700

    adding routes to get background images for carousel

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,11 @@
 
 const router = require("express").Router();
 const bookRoutes = require("./books");
+const unsplashRoutes = require("./unsplash");
 
 // Book routes
 router.use("/books", bookRoutes);
+
+router.use("/unsplash", unsplashRoutes);
 
 module.exports = router;
