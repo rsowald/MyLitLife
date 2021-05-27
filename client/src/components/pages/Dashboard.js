88b92a05bcda0cc50626b473434360b8eccbec60
Victor <VictorCodrean@users.noreply.github.com>
2021-05-26 23:30:41 -0700

    bare bone populating with result

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -9,32 +9,39 @@ import {
 } from "react-bootstrap";
 import Title from './Title';
 import "./dashboard.css";
+import Glossary from "./Glossary";
 
 export default function Dashboard() {
-    // const bookGoals = useRef();
+  // const bookGoals = useRef();
 
-    return (
-        <>
-        <Title></Title>
-        <div className="container">
+  return (
+    <>
+      <Title></Title>
+      <div className="container">
+        <Row className="d-flex justify-content-center">
+          <Col>
+            <Glossary />
+          </Col>
+        </Row>
         <Row>
-        <Col md="4">
-          <Card className="card-stats">
-            <Card.Body>
-              <Row>
-                <Col xs="2">
-                  <div className="icon-big text-left icon-warning">
-                    <i className="nc-icon nc-chart text-warning"></i>
-                  </div>
-                </Col>
-                <Col xs="10">
-                  <div className="numbers">
+          <Col md="4">
+
+            <Card className="card-stats">
+              <Card.Body>
+                <Row>
+                  <Col xs="2">
+                    <div className="icon-big text-left icon-warning">
+                      <i className="nc-icon nc-chart text-warning"></i>
+                    </div>
+                  </Col>
+                  <Col xs="10">
+                    <div className="numbers">
                       {/* {{ useRef }} */}
-                    <p className="card-category">User Book Chart Here</p>
-                    <hr></hr>
-                    <Card.Title as="h2">Book Goals</Card.Title>
-                    <hr></hr>
-                    <Card.Body>
+                      <p className="card-category">User Book Chart Here</p>
+                      <hr></hr>
+                      <Card.Title as="h2">Book Goals</Card.Title>
+                      <hr></hr>
+                      <Card.Body>
                         Goal:
                         <br></br>
                         <br></br>
@@ -42,83 +49,83 @@ export default function Dashboard() {
                         <hr></hr>
                         Currently Reading:
                     </Card.Body>
-                  </div>
-                </Col>
-              </Row>
-            </Card.Body>
-            <Card.Footer>
-              <hr></hr>
-              <div className="stats">
-                <i className="fas fa-redo mr-1"></i>
-                                <ButtonToolbar className="mt-3 justify-content-between" aria-label="Toolbar with Button groups">
-                  <Button variant="secondary" type="submit">Update</Button>
-                </ButtonToolbar>
-              </div>
-            </Card.Footer>
-          </Card>
-        </Col>
-        <Col>
-        Insert Quote Here
-        </Col>
-        <Col md="4">
-          <Card className="card-queue">
-            <Card.Body>
-              <Row>
-                <Col xs="2">
-                  <div className="icon-big text-left icon-warning">
-                    <i className="nc-icon nc-chart text-warning"></i>
-                  </div>
-                </Col>
-                <Col xs="10">
-                  <div className="numbers">
+                    </div>
+                  </Col>
+                </Row>
+              </Card.Body>
+              <Card.Footer>
+                <hr></hr>
+                <div className="stats">
+                  <i className="fas fa-redo mr-1"></i>
+                  <ButtonToolbar className="mt-3 justify-content-between" aria-label="Toolbar with Button groups">
+                    <Button variant="secondary" type="submit">Update</Button>
+                  </ButtonToolbar>
+                </div>
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col md="4">
+            Insert Quote Here
+          </Col>
+          <Col md="4">
+            <Card className="card-queue">
+              <Card.Body>
+                <Row>
+                  <Col xs="2">
+                    <div className="icon-big text-left icon-warning">
+                      <i className="nc-icon nc-chart text-warning"></i>
+                    </div>
+                  </Col>
+                  <Col xs="10">
+                    <div className="numbers">
                       {/* {{ useRef }} */}
-                    <hr></hr>
-                    <Card.Title as="h2">Book Queue</Card.Title>
-                    <hr></hr>
-                    <Card.Body>
-                         <ul>
-                        <li>Book 1</li>
-                        <li>Book 2</li>
-                        <li>Book 3</li>
-                        <li>Book 4</li>
-                        <li>Book 5</li>
-                    </ul>
-                    </Card.Body>
-                  </div>
-                </Col>
-              </Row>
-            </Card.Body>
-            <Card.Footer>
-              <hr></hr>
-              <div className="stats">
-                                <ButtonToolbar className="mt-3 justify-content-between" aria-label="Toolbar with Button groups">
-                  <Button variant="secondary" type="submit">Find Books</Button>
-                </ButtonToolbar>
-              </div>
-            </Card.Footer>
-          </Card>
-          <Row>
-        <Row>
-            <br></br>
-        </Row>
-          <Col md="12">
-          <Card className="card-completed-form">
-            <Card.Body>
+                      <hr></hr>
+                      <Card.Title as="h2">Book Queue</Card.Title>
+                      <hr></hr>
+                      <Card.Body>
+                        <ul>
+                          <li>Book 1</li>
+                          <li>Book 2</li>
+                          <li>Book 3</li>
+                          <li>Book 4</li>
+                          <li>Book 5</li>
+                        </ul>
+                      </Card.Body>
+                    </div>
+                  </Col>
+                </Row>
+              </Card.Body>
+              <Card.Footer>
+                <hr></hr>
+                <div className="stats">
+                  <ButtonToolbar className="mt-3 justify-content-between" aria-label="Toolbar with Button groups">
+                    <Button variant="secondary" type="submit">Find Books</Button>
+                  </ButtonToolbar>
+                </div>
+              </Card.Footer>
+            </Card>
+            <Row>
               <Row>
-                <Col xs="2">
-                  <div className="icon-big text-left icon-warning">
-                    <i className="nc-icon nc-chart text-warning"></i>
-                  </div>
-                </Col>
-                <Col xs="10">
-                  <div className="numbers">
-                      {/* {{ useRef }} */}
-                    <p className="card-category">User Book Chart Here</p>
-                    <hr></hr>
-                    <Card.Title as="h2">Completed Book Form</Card.Title>
-                    <hr></hr>
-                    <Card.Body>
-                        <h3 className="text-center mb-4">Sign Up</h3>
+                <br></br>
+              </Row>
+              <Col md="12">
+                <Card className="card-completed-form">
+                  <Card.Body>
+                    <Row>
+                      <Col xs="2">
+                        <div className="icon-big text-left icon-warning">
+                          <i className="nc-icon nc-chart text-warning"></i>
+                        </div>
+                      </Col>
+                      <Col xs="10">
+                        <div className="numbers">
+                          {/* {{ useRef }} */}
+                          <p className="card-category">User Book Chart Here</p>
+                          <hr></hr>
+                          <Card.Title as="h2">Completed Book Form</Card.Title>
+                          <hr></hr>
+                          <Card.Body>
+                            <h3 className="text-center mb-4">Sign Up</h3>
                             {/* {currentUser.email} */}
                             {/* {passwordError && <Alert variant="danger">{passwordError}</Alert>}
                             {firebaseError && <Alert variant="danger">{firebaseError}</Alert>}
@@ -149,27 +156,27 @@ export default function Dashboard() {
                                     : <Spinner className="mt-3" animation="border" variant="success" />
                                 }
                             </Form> */}
-                    </Card.Body>
-                  </div>
-                </Col>
-              </Row>
-            </Card.Body>
-            <Card.Footer>
-              <hr></hr>
-              <div className="stats">
-                <i className="fas fa-redo mr-1"></i>
-                                <ButtonToolbar className="mt-3 justify-content-between" aria-label="Toolbar with Button groups">
-                  <Button variant="secondary" type="submit">Update</Button>
-                </ButtonToolbar>
-              </div>
-            </Card.Footer>
-          </Card>
-        </Col>
+                          </Card.Body>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                  <Card.Footer>
+                    <hr></hr>
+                    <div className="stats">
+                      <i className="fas fa-redo mr-1"></i>
+                      <ButtonToolbar className="mt-3 justify-content-between" aria-label="Toolbar with Button groups">
+                        <Button variant="secondary" type="submit">Update</Button>
+                      </ButtonToolbar>
+                    </div>
+                  </Card.Footer>
+                </Card>
+              </Col>
+            </Row>
+
+          </Col>
         </Row>
-          
-        </Col>
-      </Row>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  )
 }
--- client/src/components/pages/Glossary.js
+++ client/src/components/pages/Glossary.js
@@ -1,11 +1,108 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Container, Row, Col, Card, Form, Button, Alert, Spinner, ListGroup } from "react-bootstrap";
+import API from "../../utils/API";
 
-function Glossary() {
+export default function Glossary() {
+    const wordRef = useRef()
+    // const [word, setWord] = useState()
+    const [errorMessage, seterrorMessage] = useState('')
+    const [btnLoading, setBtnLoading] = useState(false)
+    const [btnSpin, setBtnSpinner] = useState(false)
+    const [searchResult, setsearchResult] = useState()
+
+    async function handleSearch(event) {
+        event.preventDefault();
+        console.log("input value: ", wordRef.current.value)
+        const app_id = 'ca8c5d3b';
+        const app_key = 'd0a0ba1b62842eb7b002340c1b1b7723'
+
+        try {
+            setBtnSpinner(true)
+            setBtnLoading(true)
+            const query = wordRef.current.value
+            await API.searchInMerriamDictionary(wordRef.current.value)
+                .then(results => {
+                    // console.log(results);
+                    console.log(results.data);
+                    setsearchResult(results.data)
+                })
+
+            // API.searchInDictionary(wordRef)
+            //     .then(results => {
+            //         console.log(results);
+            //     })
+        } catch (error) {
+            console.log(error)
+            seterrorMessage(error.message)
+        }
+        setBtnLoading(false)
+        setBtnSpinner(false)
+    }
+
     return (
         <>
+            <Card className='my-3'>
+                <Row className="no-gutters vertical-divider">
+                    <h3 className="text-center mb-4">Glossary</h3>
+                    <hr />
+
+
+                    <Col md="8" >
+
+
+                        <p className="d-flex justify-content-start">
+                            Get the most trusted, up-to-date definitions from Merriam:
+                    </p>
+                        {/* {passwordError && <Alert variant="danger">{passwordError}</Alert>} */}
+                        {/* {errorMessage && <Alert variant="danger">{errorMessage}</Alert>} */}
+
+
+
+                    </Col>
+                    <Col className="border-start  border-dark" md="4">
+                        <Form onSubmit={handleSearch}>
+                            <Form.Group className="mb-3" id="word">
+                                <Form.Label className="" >Search for word:</Form.Label>
+                                <Form.Control name="word" type="text" ref={wordRef} placeholder="Ex: exponential" required />
+                            </Form.Group>
+                            <Row className="d-flex justify-content">
+                                <Col sm="12" md="2">
+                                    {!btnSpin
+                                        ? < Button disabled={btnLoading} className="mt-3" variant="success" type="submit">Search</Button>
+                                        : <Spinner className="mt-3" animation="border" variant="success" />
+                                    }
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Col>
+                </Row>
+                {searchResult &&
+                    <Row className="mt-3">
+                        <hr />
+
+                        <Col sm="12" md="6">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Header>{wordRef.current.value}</Card.Header>
+                                <ListGroup variant="flush">
+                                    {searchResult.map(res => {
+                                        return (
+                                            <>
+                                                <ListGroup.Item key={res.meta.uuid}>{res.shortdef}</ListGroup.Item>
+
+                                            </>
+                                        )
+                                    })}
+
+                                </ListGroup>
+                            </Card>
+
+                        </Col>
+                    </Row>
+                }
+
+            </Card>
         </>
     )
 
 }
 
-export default Glossary;
