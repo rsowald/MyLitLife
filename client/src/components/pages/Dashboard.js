03a22f527344b9192d6ca7522f7f081eb25e6fe1
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-27 21:52:35 -0700

    fixing yellow color hijacking every card

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -11,30 +11,30 @@ import Title from './Title';
 import "./dashboard.css";
 
 export default function Dashboard() {
-    // const bookGoals = useRef();
+  // const bookGoals = useRef();
 
-    return (
-        <>
-        <Title></Title>
-        <div className="container">
+  return (
+    <>
+      <Title />
+      <div className="container">
         <Row>
-        <Col md="4">
-          <Card className="card-stats">
-            <Card.Body>
-              <Row>
-                <Col xs="2">
-                  <div className="icon-big text-left icon-warning">
-                    <i className="nc-icon nc-chart text-warning"></i>
-                  </div>
-                </Col>
-                <Col xs="10">
-                  <div className="numbers">
+          <Col md="4">
+            <Card className="card-stats" style={{ backgroundColor: "#f7d065" }}>
+              <Card.Body>
+                <Row>
+                  <Col xs="2">
+                    <div className="icon-big text-left icon-warning">
+                      <i className="nc-icon nc-chart text-warning"></i>
+                    </div>
+                  </Col>
+                  <Col xs="10">
+                    <div className="numbers">
                       {/* {{ useRef }} */}
-                    <p className="card-category">User Book Chart Here</p>
-                    <hr></hr>
-                    <Card.Title as="h2">Book Goals</Card.Title>
-                    <hr></hr>
-                    <Card.Body>
+                      <p className="card-category">User Book Chart Here</p>
+                      <hr></hr>
+                      <Card.Title as="h2">Book Goals</Card.Title>
+                      <hr></hr>
+                      <Card.Body>
                         Goal:
                         <br></br>
                         <br></br>
@@ -42,83 +42,83 @@ export default function Dashboard() {
                         <hr></hr>
                         Currently Reading:
                     </Card.Body>
-                  </div>
-                </Col>
-              </Row>
-            </Card.Body>
-            <Card.Footer>
-              <hr></hr>
-              <div className="stats">
-                <i className="fas fa-redo mr-1"></i>
-                                <ButtonToolbar className="mt-3 justify-content-between" aria-label="Toolbar with Button groups">
-                  <Button variant="secondary" type="submit">Update</Button>
-                </ButtonToolbar>
-              </div>
-            </Card.Footer>
-          </Card>
+                    </div>
+                  </Col>
+                </Row>
+              </Card.Body>
+              <Card.Footer>
+                <hr></hr>
+                <div className="stats">
+                  <i className="fas fa-redo mr-1"></i>
+                  <ButtonToolbar className="mt-3 justify-content-between" aria-label="Toolbar with Button groups">
+                    <Button variant="secondary" type="submit">Update</Button>
+                  </ButtonToolbar>
+                </div>
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            Insert Quote Here
         </Col>
-        <Col>
-        Insert Quote Here
-        </Col>
-        <Col md="4">
-          <Card className="card-queue">
-            <Card.Body>
-              <Row>
-                <Col xs="2">
-                  <div className="icon-big text-left icon-warning">
-                    <i className="nc-icon nc-chart text-warning"></i>
-                  </div>
-                </Col>
-                <Col xs="10">
-                  <div className="numbers">
+          <Col md="4">
+            <Card className="card-queue" style={{ backgroundColor: "#f7d065" }}>
+              <Card.Body>
+                <Row>
+                  <Col xs="2">
+                    <div className="icon-big text-left icon-warning">
+                      <i className="nc-icon nc-chart text-warning"></i>
+                    </div>
+                  </Col>
+                  <Col xs="10">
+                    <div className="numbers">
                       {/* {{ useRef }} */}
-                    <hr></hr>
-                    <Card.Title as="h2">Book Queue</Card.Title>
-                    <hr></hr>
-                    <Card.Body>
-                         <ul>
-                        <li>Book 1</li>
-                        <li>Book 2</li>
-                        <li>Book 3</li>
-                        <li>Book 4</li>
-                        <li>Book 5</li>
-                    </ul>
-                    </Card.Body>
-                  </div>
-                </Col>
+                      <hr></hr>
+                      <Card.Title as="h2">Book Queue</Card.Title>
+                      <hr></hr>
+                      <Card.Body>
+                        <ul>
+                          <li>Book 1</li>
+                          <li>Book 2</li>
+                          <li>Book 3</li>
+                          <li>Book 4</li>
+                          <li>Book 5</li>
+                        </ul>
+                      </Card.Body>
+                    </div>
+                  </Col>
+                </Row>
+              </Card.Body>
+              <Card.Footer>
+                <hr></hr>
+                <div className="stats">
+                  <ButtonToolbar className="mt-3 justify-content-between" aria-label="Toolbar with Button groups">
+                    <Button variant="secondary" type="submit">Find Books</Button>
+                  </ButtonToolbar>
+                </div>
+              </Card.Footer>
+            </Card>
+            <Row>
+              <Row>
+                <br></br>
               </Row>
-            </Card.Body>
-            <Card.Footer>
-              <hr></hr>
-              <div className="stats">
-                                <ButtonToolbar className="mt-3 justify-content-between" aria-label="Toolbar with Button groups">
-                  <Button variant="secondary" type="submit">Find Books</Button>
-                </ButtonToolbar>
-              </div>
-            </Card.Footer>
-          </Card>
-          <Row>
-        <Row>
-            <br></br>
-        </Row>
-          <Col md="12">
-          <Card className="card-completed-form">
-            <Card.Body>
-              <Row>
-                <Col xs="2">
-                  <div className="icon-big text-left icon-warning">
-                    <i className="nc-icon nc-chart text-warning"></i>
-                  </div>
-                </Col>
-                <Col xs="10">
-                  <div className="numbers">
-                      {/* {{ useRef }} */}
-                    <p className="card-category">User Book Chart Here</p>
-                    <hr></hr>
-                    <Card.Title as="h2">Completed Book Form</Card.Title>
-                    <hr></hr>
-                    <Card.Body>
-                        <h3 className="text-center mb-4">Sign Up</h3>
+              <Col md="12">
+                <Card className="card-completed-form" style={{ backgroundColor: "#f7d065" }}>
+                  <Card.Body>
+                    <Row>
+                      <Col xs="2">
+                        <div className="icon-big text-left icon-warning">
+                          <i className="nc-icon nc-chart text-warning"></i>
+                        </div>
+                      </Col>
+                      <Col xs="10">
+                        <div className="numbers">
+                          {/* {{ useRef }} */}
+                          <p className="card-category">User Book Chart Here</p>
+                          <hr></hr>
+                          <Card.Title as="h2">Completed Book Form</Card.Title>
+                          <hr></hr>
+                          <Card.Body>
+                            <h3 className="text-center mb-4">Sign Up</h3>
                             {/* {currentUser.email} */}
                             {/* {passwordError && <Alert variant="danger">{passwordError}</Alert>}
                             {firebaseError && <Alert variant="danger">{firebaseError}</Alert>}
@@ -149,27 +149,27 @@ export default function Dashboard() {
                                     : <Spinner className="mt-3" animation="border" variant="success" />
                                 }
                             </Form> */}
-                    </Card.Body>
-                  </div>
-                </Col>
-              </Row>
-            </Card.Body>
-            <Card.Footer>
-              <hr></hr>
-              <div className="stats">
-                <i className="fas fa-redo mr-1"></i>
-                                <ButtonToolbar className="mt-3 justify-content-between" aria-label="Toolbar with Button groups">
-                  <Button variant="secondary" type="submit">Update</Button>
-                </ButtonToolbar>
-              </div>
-            </Card.Footer>
-          </Card>
-        </Col>
+                          </Card.Body>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                  <Card.Footer>
+                    <hr></hr>
+                    <div className="stats">
+                      <i className="fas fa-redo mr-1"></i>
+                      <ButtonToolbar className="mt-3 justify-content-between" aria-label="Toolbar with Button groups">
+                        <Button variant="secondary" type="submit">Update</Button>
+                      </ButtonToolbar>
+                    </div>
+                  </Card.Footer>
+                </Card>
+              </Col>
+            </Row>
+
+          </Col>
         </Row>
-          
-        </Col>
-      </Row>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  )
 }
