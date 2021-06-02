165780bbf5523e23c4a22c11bd0bb871c371bb01
rsowald <72467920+rsowald@users.noreply.github.com>
2021-06-02 11:19:21 -0700

    background image takes up full screen

--- client/src/components/Background.js
+++ client/src/components/Background.js
@@ -2,15 +2,20 @@ import React, { useEffect, useState } from "react";
 import API from '../utils/API';
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
+import useWindowSize from "../utils/windowWidth";
+import "./background.css";
 
 function Background() {
     const [images, setImages] = useState([]);
     const [imageIndex, setImageIndex] = useState(0);
+    const [portrait, setPortrait] = useState(false);
+    const windowSize = useWindowSize();
 
     useEffect(() => {
+
         async function callAPI() {
             try {
-                const backgroundsRaw = await API.getBackground();
+                const backgroundsRaw = await API.getBackground(windowSize);
                 const backgrounds = backgroundsRaw.data.map((picture) => ({
                     blur_hash: picture.blur,
                     original: picture.image,
@@ -23,7 +28,11 @@ function Background() {
             }
         }
         callAPI();
-    }, []);
+    }, [portrait]);
+
+    useEffect(() => {
+        windowSize > 480 ? setPortrait(false) : setPortrait(true)
+    }, [windowSize]);
 
     const currentImage = images[imageIndex];
 
@@ -35,6 +44,8 @@ function Background() {
                 autoPlay={true}
                 showThumbnails={false}
                 infinite={true}
+                fullscreen={true}
+                sizes="(min-width: 36em) 33.3vw, 100vw"
                 showPlayButton={false}
                 showFullscreenButton={false}
                 disableKeyDown={true}
@@ -47,7 +58,7 @@ function Background() {
             { currentImage &&
                 <span className="text-muted">Photo by&nbsp;
                    <a href={`${currentImage.profile}?utm_source=my-lit-life&utm_medium=referral`} target="_blank" rel="noopener noreferrer">{currentImage.artist}</a>
-                   &nbsp;on <a href="https://unsplash.com/?utm_source=my-lit-life&utm_medium=referral" target="_blank" rel="noopener noreferrer">Unsplash</a>
+                   &nbsp;on <a href="https://unsplash.com/?utm_source=my-lit-life&amputm_medium=referral" target="_blank" rel="noopener noreferrer">Unsplash</a>
                 </span>
             }
         </>
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import "react-image-gallery/styles/css/image-gallery.css";
@@ -6,33 +6,47 @@ import Background from "../Background";
 import Login from "../authentication/Login";
 import Signup from "../authentication/Signup";
 import SidePanel from "../SidePanel";
+import useWindowSize from "../../utils/windowWidth";
 
 function Home() {
     const location = useLocation();
     const { defaultShowLogin, defaultShowSignup } = location.state || {};
     const [showLogin, setShowLogin] = useState(defaultShowLogin);
     const [showSignup, setShowSignup] = useState(defaultShowSignup);
+    const [portrait, setPortrait] = useState(false);
+    const windowSize = useWindowSize();
+
+    useEffect(() => {
+        windowSize > 480 ? setPortrait(false) : setPortrait(true)
+    }, [windowSize]);
 
     return (
         <Container fluid style={{ position: "relative" }}>
 
             <Row>
                 <Col size="md-8" className="d-inline text-center"
-                    style={{
+                    style={portrait ? {
+                        backgroundColor: "rgba(247, 190, 2, .8)",
+                        borderRadius: "5px",
+                        position: "absolute",
+                        zIndex: 1,
+                        left: "13%",
+                        top: "2%",
+                        width: "75%"
+                    } : {
                         backgroundColor: "rgba(247, 190, 2, .8)",
                         borderRadius: "5px",
                         position: "absolute",
                         zIndex: 1,
                         left: "25%",
-                        top: "20%",
-                        width: "50%"
+                        top: "50%",
+                        width: "35%"
                     }}>
-                    <h1>Welcome to My Lit Life:</h1>
-                    <h2>your personal literary goal tracker.</h2>
-                    <p>Please <Button variant="danger" onClick={() => setShowLogin(true)}>login</Button> to get started.</p>
+                    <h2>Welcome to your personal literary goal tracker.</h2>
+                    <p>Please <Button variant="danger" onClick={() => setShowLogin(true)}><i className="fas fa-unlock-alt" />  log in</Button> to get started.</p>
                 </Col>
             </Row>
-            <Background style={{ position: "absolute", zIndex: 0 }} />
+            <Background style={{ width: "100vw", height: "100vh", position: "absolute", zIndex: 0 }} />
             <SidePanel isOpen={showLogin || showSignup} onClose={() => { setShowLogin(false); setShowSignup(false); }}>
                 {!showSignup ?
                     <Login onClick={() => setShowSignup(true)} /> :
