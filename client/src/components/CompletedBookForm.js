224e7cee0b8ada5d8982ee82d5ba7d628b0aa7df
rsowald <72467920+rsowald@users.noreply.github.com>
2021-06-02 00:09:29 -0700

    completed book form saving to db

--- client/src/components/CompletedBookForm.js
+++ client/src/components/CompletedBookForm.js
@@ -1,5 +1,5 @@
-import React, { useRef } from "react";
-import { Form, Button } from 'react-bootstrap';
+import React, { useRef, useState } from "react";
+import { Form, Button, Toast } from 'react-bootstrap';
 import API from "../utils/API";
 import { useAuth } from "../components/authentication/context/AuthContext";
 
@@ -7,38 +7,46 @@ function BookForm() {
     const { currentUser } = useAuth();
     const titleRef = useRef();
     const authorRef = useRef();
-    const pagesRef = useRef();
-    const book = {VolumeInfo:{
-      title: titleRef, 
-      author: authorRef,
-      pages: pagesRef
-    }}
 
-    function handleAddedBook(event) {
-    event.preventDefault();
+    const [showToast, setShowToast] = useState(false);
 
-    API.addToCompleted(book, currentUser.uid)
-      .catch(err => {
-        console.log(err)
-      });
-      }
+    const handleAddedBook = (event) => {
+        event.preventDefault();
+        API.searchBooks(`intitle:${titleRef.current.value}+inauthor:${authorRef.current.value}`)
+            .then(res => {
+                if (res.data.items) {
+                    API.addToCompleted(res.data.items[0], currentUser.uid)
+                }
+                else {
+                    setShowToast(true);
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            });
+        titleRef.current.value = "";
+        authorRef.current.value = "";
+    };
 
-  return (
-      <>
-                            <Form onSubmit={()=>handleAddedBook}>
-                                <Form.Group className="mb-3" id="title">
-                                    <Form.Control name="title" type="name" placeholder="Book Title" ref={titleRef} required />
-                                </Form.Group>
-                                <Form.Group className="mb-3" id="author">
-                                    <Form.Control name="author" type="name" placeholder="Author" ref={authorRef} required />
-                                </Form.Group>
-                                <Form.Group className="mb-3" id="email">
-                                    <Form.Control name="pages" type="number" placeholder="Pages" ref={pagesRef} required />
-                                </Form.Group>
-                                <Button type="submit">Add Book!</Button>
-                                </Form>
-       </>
-    )
+    return (
+        <>
+            <Form onSubmit={handleAddedBook} >
+                <Form.Group className="mb-3" id="title">
+                    <Form.Control name="title" type="name" placeholder="Book Title" ref={titleRef} required />
+                </Form.Group>
+                <Form.Group className="mb-3" id="author">
+                    <Form.Control name="author" type="name" placeholder="Author" ref={authorRef} required />
+                </Form.Group>
+                <Button type="submit">Add Book!</Button>
+            </Form>
+            <Toast className="mt-3" show={showToast} onClose={() => setShowToast(false)}>
+                <Toast.Header>
+                    <strong className="mr-auto">Oh No! Try again.</strong>
+                </Toast.Header>
+                <Toast.Body>We couldn't find a book by that title and author.</Toast.Body>
+            </Toast>
+        </>
+    );
 };
 
 export default BookForm;
