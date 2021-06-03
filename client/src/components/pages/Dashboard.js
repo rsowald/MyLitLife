d00e11ae319010eb30195fba141c1218fe119117
rsowald <72467920+rsowald@users.noreply.github.com>
2021-06-03 02:02:47 -0700

    error handing for completed book dashboard form

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 // react-bootstrap components
 import { Container, Card, Row, Col, Button, ButtonToolbar } from "react-bootstrap";
 import Title from '../Title';
@@ -6,13 +6,20 @@ import "./dashboard.css";
 import Glossary from "../Glossary";
 import DashboardQuoteCol from "../DashboardQuoteCol";
 import BestSellers from '../BestSellers';
-import BookForm from '../CompletedBookForm';
+import CompletedBookForm from '../CompletedBookForm';
 import History from '../History';
 import BookGoalChart from "../BookGoalChart";
 import PageGoalChart from "../PageGoalChart";
+import RecentlyCompleted from "../RecentlyCompleted";
 
 export default function Dashboard() {
   // const bookGoals = useRef();
+
+  let refresher = null;
+
+  const onNewCompletedBook = () => {
+    refresher();
+  };
 
   return (
     <>
@@ -21,6 +28,8 @@ export default function Dashboard() {
         <Row className="d-flex justify-content-center mb-3">
           <Col>
             <Card className="card-stats" style={{ backgroundColor: "#f7d065" }}>
+              <Card.Title as="h2">Book Goals</Card.Title>
+              <hr></hr>
               <Card.Body>
                 <Row>
                   <Col xs="2">
@@ -37,101 +46,41 @@ export default function Dashboard() {
                   <Col xs={3}>
                     <PageGoalChart className="chart" />
                   </Col>
-                      {/* {{ useRef }} */}
-                      <hr></hr>
-                      <Card.Title as="h2">Book Goals</Card.Title>
-                      <hr></hr>
-                      <Card.Body>
-                        Goal:
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <hr></hr>
+                  {/* {{ useRef }} */}
+                  <hr></hr>
+
+                  <Card.Body>
+                    Goal:
+                        {/* input */}
+                    <hr></hr>
                         Currently Reading:
-                    </Card.Body>
+                        {/* dropdown of book queue */}
+                  </Card.Body>
                 </Row>
               </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  <i className="fas fa-redo mr-1"></i>
-                  <ButtonToolbar className="mt-3 justify-content-between" aria-label="Toolbar with Button groups">
-                    <Button variant="secondary" type="submit">Update</Button>
-                  </ButtonToolbar>
-                </div>
-              </Card.Footer>
             </Card>
           </Col>
         </Row>
         <Row>
           <Col md="4">
             <Glossary />
-
           </Col>
           <Col>
             <DashboardQuoteCol />
           </Col>
           <Col md="4">
-            <Card className="card-queue" style={{ backgroundColor: "#f7d065" }}>
-              <Card.Body>
-                <Row>
-                  <Col xs="2">
-                    <div className="icon-big text-left icon-warning">
-                      <i className="nc-icon nc-chart text-warning"></i>
-                    </div>
-                  </Col>
-                  <Col xs="10">
-                    <div className="numbers">
-                      {/* {{ useRef }} */}
-                      <hr></hr>
-                      <Card.Title as="h2">Book Queue</Card.Title>
-                      <hr></hr>
-                      <Card.Body>
-                        <ul>
-                          <li>Book 1</li>
-                          <li>Book 2</li>
-                          <li>Book 3</li>
-                          <li>Book 4</li>
-                          <li>Book 5</li>
-                        </ul>
-                      </Card.Body>
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  <ButtonToolbar className="mt-3 justify-content-between" aria-label="Toolbar with Button groups">
-                    <Button variant="secondary" type="submit">Find Books</Button>
-                  </ButtonToolbar>
-                </div>
-              </Card.Footer>
-            </Card>
+            <RecentlyCompleted refresher={(fn) => refresher = fn} />
             <Row>
               <Row>
                 <br></br>
               </Row>
               <Col md="12">
-                <Card className="card-completed-form" style={{ backgroundColor: "#f7d065" }}>
+                <Card style={{ backgroundColor: "#f7d065" }}>
                   <Card.Body>
-                    <Row>
-                      <Col lg="2">
-                        <div className="icon-big text-left icon-warning">
-                          <i className="nc-icon nc-chart text-warning"></i>
-                        </div>
-                      </Col>
-                      <Col lg="10">
-                        <div className="numbers">
-                          <Card.Title as="h2">Completed Book Form</Card.Title>
-                          <Card.Body>
-                            <h6 className="text-center mb-9">Add Your Book Below!</h6>
-                            <hr></hr>
-                            <BookForm />
-                          </Card.Body>
-                        </div>
-                      </Col>
-                    </Row>
+                    <Card.Title as="h2">Completed Book Form</Card.Title>
+                    <h6 className="text-center mb-9">Add Your Book Below!</h6>
+                    <hr></hr>
+                    <CompletedBookForm onAdd={onNewCompletedBook} />
                   </Card.Body>
                 </Card>
               </Col>
