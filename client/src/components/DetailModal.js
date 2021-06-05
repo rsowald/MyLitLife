9c3d30b24c63cc9f77b7e36cb615b743b491f6e7
rsowald <72467920+rsowald@users.noreply.github.com>
2021-06-04 22:38:27 -0700

    added star rating npm package and styled modal

--- client/src/components/DetailModal.js
+++ client/src/components/DetailModal.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Button, Form, Modal } from "react-bootstrap";
+import { Col, Button, Form, Modal } from "react-bootstrap";
 import { useBookModal } from "../context/ModalContext";
 import API from "../utils/API";
-import Rating from "./Rating";
+import StarRatings from 'react-star-ratings';
 import { useAuth } from "./authentication/context/AuthContext";
 
 
 function DetailModal() {
     const { currentUser } = useAuth();
     const { bookModalInfo, hideModal } = useBookModal();
-    const [rating, setRating] = useState();
+    const [rating, setRating] = useState(0);
     const [book, setBook] = useState();
     const notesRef = useRef();
     const reviewRef = useRef();
@@ -25,7 +25,7 @@ function DetailModal() {
             const bookRes = await bookAPI(bookModalInfo.bookId, currentUser.uid);
             const book = bookRes.data;
             setBook(book);
-            setRating(book.rating);
+            setRating(book.rating || 0);
             notesRef.current.value = book.notes || '';
             reviewRef.current.value = book.review || '';
             endingRef.current.value = book.ending || '';
@@ -54,29 +54,56 @@ function DetailModal() {
             aria-labelledby="contained-modal-title-vcenter"
             centered
         >
-            <Modal.Header closeButton>
+            <Modal.Header style={{ backgroundColor: "#f7d065", color: "saddlebrown" }}>
                 <Modal.Title id="contained-modal-title-vcenter">
-                    <h2>{book && book.volumeInfo.title}</h2>
+                    <h2 className="d-inline-flex mr-3">{book && book.volumeInfo.title}</h2><p className="d-inline-flex">by {book && book.volumeInfo.authors[0]}</p>
                 </Modal.Title>
             </Modal.Header>
             <Form onSubmit={onSave}>
-                <Modal.Body>
-                    <p text="muted" className="">by {book && book.volumeInfo.authors[0]}</p>
-                    <label className="mt-4">My rating
-                        <Rating onChange={setRating} rating={rating} />
-                    </label>
-                    <label className="mt-4">Reading Notes
-                        <textarea name="notes" ref={notesRef} />
-                    </label>
-                    <label className="mt-4">My Review
-                        <textarea name="review" ref={reviewRef} />
-                    </label>
-                    <label className="mt-4">How it Should have Ended
-                        <textarea name="ending" ref={endingRef} />
-                    </label>
+                <Modal.Body style={{ color: "saddlebrown" }}>
+                    <Form.Row>
+                        <Form.Label className="mr-5" >My rating</Form.Label>
+                        <Col>
+                            <StarRatings style={{ marginLeft: "10px" }}
+                                rating={rating}
+                                starRatedColor="red"
+                                changeRating={setRating}
+                                numberOfStars={5}
+                                name='rating'
+                            />
+                        </Col>
+                    </Form.Row>
+                    <br />
+                    <Form.Row>
+                        <Form.Label column lg={2}>
+                            Reading Notes
+                        </Form.Label>
+                        <Col>
+                            <Form.Control as="textarea" type="text" ref={notesRef} placeholder="Passages, marginalia, or words to remember." />
+                        </Col>
+                    </Form.Row>
+                    <br />
+                    <Form.Row>
+                        <Form.Label column lg={2}>
+                            My Review
+                        </Form.Label>
+                        <Col>
+                            <Form.Control as="textarea" type="text" ref={reviewRef} placeholder="Synopsis and thoughts." />
+                        </Col>
+                    </Form.Row>
+                    <br /><Form.Row>
+                        <Form.Label column lg={2}>
+                            How it Should have Ended
+                        </Form.Label>
+                        <Col>
+                            <Form.Control as="textarea" type="text" ref={endingRef} placeholder="Would you have done it differently?" />
+                        </Col>
+                    </Form.Row>
+                    <br />
+                    <a href={book && book.volumeInfo.infoLink} target="_blank" rel="noopener noreferrer">See on Google Books</a>
                 </Modal.Body>
-                <Modal.Footer>
-                    <Button type="submit">Save &amp; Close</Button>
+                <Modal.Footer style={{ backgroundColor: "#f7d065" }}>
+                    <Button variant="secondary" type="submit">Save &amp; Close</Button>
                 </Modal.Footer>
             </Form>
         </Modal>
