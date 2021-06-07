cac883348e9dc57336aad46d172925a7fc9160c5
rsowald <72467920+rsowald@users.noreply.github.com>
2021-06-07 00:36:01 -0700

    goals pull from user data

--- client/src/components/BookGoalChart.js
+++ client/src/components/BookGoalChart.js
@@ -1,67 +1,135 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Card } from 'react-bootstrap';
+import API from "../utils/API";
+import { useAuth } from "./authentication/context/AuthContext";
+import { useUser } from "../context/UserContext";
 
-class BookGoalChart extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            chartData: {
-                labels: ['January', 'February','March','April','May','June'],
-                datasets:[
-                    {
-                        label: 'Books',
-                        data:[
-                            14,
-                            10,
-                            15,
-                            17,
-                            11,
-                            0
-                        ],
-                        backgroundColor:'#ff9f40'
-                
-                    },
-                    {
-                        label: 'Goal',
-                        data:[
-                            12,
-                            12,
-                            12,
-                            12,
-                            12,
-                            12
-                        ],
-                        backgroundColor:"saddlebrown"
-                
-                    }
 
-                ]
+function BookGoalChart(props) {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var date = new Date();
+    const { currentUser } = useAuth();
+    const firebaseUser = currentUser.uid;
+    const currentMonth = date.getMonth();
+    const currentYear = date.getFullYear();
+
+    const [labels, setLabels] = useState([]);
+    const [books, setBooks] = useState([]);
+    const [bookGoal, setBookGoal] = useState(0);
+    const { user } = useUser();
+
+    function getLabels(count) {
+        var mos = [];
+        for (var i = 0; i < count; i++) {
+            let mo = currentMonth - i;
+            if (mo < 0) {
+                mo += 12;
             }
+            mos.push(months[mo])
         }
+        return mos;
+    };
+
+    function monthsAgo(date, max) {
+        const timestamp = new Date(date);
+        const yearDiff = currentYear - timestamp.getFullYear();
+        var monDiff = currentMonth - timestamp.getMonth();
+        switch (yearDiff) {
+            case 0:
+                if (monDiff < max) {
+                    return monDiff;
+                }
+                return false;
+            case 1:
+                if (monDiff < 0) {
+                    monDiff += 12;
+                }
+                if (monDiff < max) {
+                    return monDiff;
+                }
+                return false;
+            default:
+                return false;
+        }
+    };
+
+    function getBooks(max) {
+        let BookTotals = [];
     }
-    render(){
-        return (
-            <Card>
+
+    const numberOfMonthsBack = 6;
+
+    useEffect(() => {
+        setLabels(getLabels(numberOfMonthsBack));
+    }, []);
+
+    useEffect(() => {
+        let goal = [];
+        const booksPerMonth = user.bookGoal / 12;
+        for (var i = 0; i < numberOfMonthsBack; i++) {
+            goal.push(booksPerMonth)
+        }
+        setBookGoal(goal);
+    }, [user]);
+
+    const chartData = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Books',
+                data: [
+                    3,
+                    3,
+                    4,
+                    2,
+                    2,
+                    0
+                ],
+                backgroundColor: '#ff9f40'
+
+            },
+            {
+                label: 'Goal',
+                data: bookGoal,
+                // data: [
+                //     12,
+                //     12,
+                //     12,
+                //     12,
+                //     12,
+                //     12
+                // ],
+                backgroundColor: "saddlebrown"
+
+            }
+
+        ]
+    }
+
+
+
+    return (
+        <Card>
             <div className="chart" style={{ backgroundColor: "#FAF9F6" }} >
                 <Bar
-                  data={this.state.chartData}
-                  options={{
-                      title: {
-                          display: true,
-                          text: "Page Count by Genre",
-                          fontSize: 25
-                      },
-                      legend: {
-                          display: true,
-                          position: "right"
-                      }
-                  }}
+                    data={chartData}
+                    options={{
+                        title: {
+                            display: true,
+                            text: "Books to Read",
+                            fontSize: 25
+                        },
+                        legend: {
+                            display: true,
+                            position: "right"
+                        }
+                    }}
                 />
             </div>
-            </Card>
-        )
-    }
+        </Card>
+    )
+
 }
 
 export default BookGoalChart;
--- client/src/components/PageGoalChart.js
+++ client/src/components/PageGoalChart.js
@@ -2,29 +2,32 @@ import React, { useState, useEffect } from 'react';
 import { Bar } from 'react-chartjs-2';
 import API from "../utils/API";
 import { useAuth } from "./authentication/context/AuthContext";
+import { useUser } from "../context/UserContext";
 import { Card } from 'react-bootstrap';
 
 function PageGoalChart() {
-    const months = ['January', 'February','March','April','May','June','July','August','September','October','November','December'];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     var date = new Date();
     const { currentUser } = useAuth();
-    const user = currentUser.uid;
+    const firebaseUser = currentUser.uid;
     const currentMonth = date.getMonth();
     const currentYear = date.getFullYear();
 
     const [labels, setLabels] = useState([]);
     const [pages, setPages] = useState([]);
+    const [pageGoal, setPageGoal] = useState(0);
+    const { user } = useUser();
 
-    function getLabels (count){
+    function getLabels(count) {
         var mos = [];
-        for (var i = 0; i < count; i++){
+        for (var i = 0; i < count; i++) {
             let mo = currentMonth - i;
-            if (mo < 0){
+            if (mo < 0) {
                 mo += 12;
             }
             mos.push(months[mo])
         }
-        return mos;   
+        return mos;
     }
 
     function monthsAgo(date, max) {
@@ -50,79 +53,90 @@ function PageGoalChart() {
         }
     }
 
-    function getPages (max){
+    function getPages(max) {
         var pageTotals = [];
-        for (var i = 0; i < max; i++){
+        for (var i = 0; i < max; i++) {
             pageTotals.push(0);
         }
         var list = {};
-        API.getCompleted(user)
-          .then(res => {
-            list = res.data;
-            list.map((book) => {
-                const ma = monthsAgo(book.createdAt, max);
-                if(ma >= 0){
-                    pageTotals[ma] += book.volumeInfo.pageCount;
-                }
+        API.getCompleted(firebaseUser)
+            .then(res => {
+                list = res.data;
+                return list.map((book) => {
+                    const ma = monthsAgo(book.createdAt, max);
+                    if (ma >= 0) {
+                        pageTotals[ma] += book.volumeInfo.pageCount;
+                    }
+                })
             })
-          })
-          .catch((err) => console.log(err));
-          return pageTotals;   
+            .catch((err) => console.log(err));
+        return pageTotals;
     }
 
+    const numberOfMonthsBack = 6;
+
     useEffect(() => {
-        const numberOfMonthsBack = 6;
-        setLabels(getLabels (numberOfMonthsBack));
+        setLabels(getLabels(numberOfMonthsBack));
         setPages(getPages(numberOfMonthsBack));
     }, []);
-  
-    const chartData = {
-                labels: labels,
-                datasets:[
-                    {
-                        label: 'Pages',
-  //                      data: pages,
-data: [4567,5678,6789,6789,5678,4567],
-backgroundColor: '#ff9f40'
-                
-                    },
-                    {
-                        label: 'Goal',
-                        data:[
-                            5000,
-                            5000,
-                            5000,
-                            5000,
-                            5000,
-                            5000
-                        ],
-                        backgroundColor:"saddlebrown"
-                
-                    }
 
-                ]
+    useEffect(() => {
+        let goal = [];
+        const pagesPerMonth = user.pageGoal / 12;
+        for (var i = 0; i < numberOfMonthsBack; i++) {
+            goal.push(pagesPerMonth)
+        }
+        setPageGoal(goal);
+    }, [user]);
+
+    const chartData = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Pages',
+                data: pages,
+                // data: [4567, 5678, 6789, 6789, 5678, 4567],
+                backgroundColor: '#ff9f40'
+
+            },
+            {
+                label: 'Goal',
+                data: pageGoal,
+                // data: [
+                //     5000,
+                //     5000,
+                //     5000,
+                //     5000,
+                //     5000,
+                //     5000
+                // ],
+                backgroundColor: "saddlebrown"
+
             }
 
-        return (
-            <Card>
+        ]
+    }
+
+    return (
+        <Card>
             <div className="chart" style={{ backgroundColor: "#FAF9F6" }} >
                 <Bar
-                  data={ chartData }
-                  options={{
-                      title: {
-                          display: true,
-                          text: "Page Count by Genre",
-                          fontSize: 25
-                      },
-                      legend: {
-                          display: true,
-                          position: "right"
-                      }
-                  }}
+                    data={chartData}
+                    options={{
+                        title: {
+                            display: true,
+                            text: "Pages to Read",
+                            fontSize: 25
+                        },
+                        legend: {
+                            display: true,
+                            position: "right"
+                        }
+                    }}
                 />
             </div>
-            </Card>
-        )
+        </Card>
+    )
 }
 
 export default PageGoalChart;
