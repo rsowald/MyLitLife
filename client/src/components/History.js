22ec370a99b02acbcff58c2db17c832d6a7a95d6
rsowald <72467920+rsowald@users.noreply.github.com>
2021-06-06 22:55:44 -0700

    added a 'not specified' category so chart wouldn't break

--- client/src/components/History.js
+++ client/src/components/History.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {Pie} from 'react-chartjs-2';
+import { Pie } from 'react-chartjs-2';
 import API from "../utils/API";
 import { useAuth } from "./authentication/context/AuthContext";
 
@@ -7,91 +7,97 @@ function History() {
     const { currentUser } = useAuth();
     const user = currentUser.uid;
 
-    
+
     const [labels, setLabels] = useState([]);
     const [pages, setPages] = useState([]);
 
     function loadData(setLabels, setPages) {
-      var list = {};
-      API.getCompleted(user)
-        .then(res => {
-          list = res.data;
-          const labels = [];
-          var pages = [];
-          list.map(book => {
-            if(labels.includes(book.volumeInfo.categories[0])){
-                  pages[labels.indexOf(book.volumeInfo.categories[0])] += book.volumeInfo.pageCount;
-              } else {
-                  labels.push(book.volumeInfo.categories[0]);
-                  pages.push(book.volumeInfo.pageCount);
-                
-              }
-              return true;
-          })
-          setLabels(labels);
-          setPages(pages); 
-        })
-        .catch((err) => console.log(err));
+        var list = {};
+        API.getCompleted(user)
+            .then(res => {
+                list = res.data;
+                const labels = [];
+                var pages = [];
+                list.map(book => {
+                    if (book.volumeInfo.categories) {
+                        if (labels.includes(book.volumeInfo.categories[0])) {
+                            pages[labels.indexOf(book.volumeInfo.categories[0])] += book.volumeInfo.pageCount;
+                        } else {
+                            labels.push(book.volumeInfo.categories[0]);
+                            pages.push(book.volumeInfo.pageCount);
+                        }
+                        return true;
+                    }
+                    else {
+                        labels.push("Not specified");
+                        pages.push(book.volumeInfo.pageCount);
+                    }
+                    return true;
+                })
+                setLabels(labels);
+                setPages(pages);
+            })
+            .catch((err) => console.log(err));
     }
-  
+
     useEffect(() => {
-      loadData(setLabels, setPages);
+        loadData(setLabels, setPages);
     }, []);
 
-        const chartData = {
-                labels: labels,
-                datasets:[
-                    {
-                        data: pages,
-                        backgroundColor: ['#ff3d67', '#ff9f40', '#ffcd56', '#4bc0c0', '#999999', '#333fde'],
-                    }
-                ],
-                showDatapoints: true,
-                options: {
-                    tooltips: {
-                        enabled: false
-                    },
-                    pieceLabel: {
-                        render: 'label',
-                        arc: true,
-                        fontColor: '#000',
-                        position: 'outside'
-                    },
-                    responsive: true,
-                    legend: {
-                        position: 'bottom',
-                    },
+    const chartData = {
+        labels: labels,
+        datasets: [
+            {
+                data: pages,
+                backgroundColor: ['#ff3d67', '#ff9f40', '#ffcd56', '#4bc0c0', '#999999', '#333fde'],
+            }
+        ],
+        showDatapoints: true,
+        options: {
+            tooltips: {
+                enabled: false
+            },
+            pieceLabel: {
+                render: 'label',
+                arc: true,
+                fontColor: '#000',
+                position: 'outside'
+            },
+            responsive: true,
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'History',
+                fontSize: 20
+            },
+            animation: {
+                animateScale: true,
+                animateRotate: true
+            }
+        }
+
+    }
+
+    return (
+        <div className="chart">
+            <Pie
+                data={chartData}
+                options={{
                     title: {
                         display: true,
-                        text: 'History',
-                        fontSize: 20
+                        text: "Page Count by Genre",
+                        fontSize: 25
                     },
-                    animation: {
-                        animateScale: true,
-                        animateRotate: true
+                    legend: {
+                        display: true,
+                        position: "right"
                     }
-                }
-    
-            }
-
-        return (
-            <div className="chart">
-                <Pie
-                   data={chartData}
-                  options={{
-                      title: {
-                          display: true,
-                          text: "Page Count by Genre",
-                          fontSize: 25
-                      },
-                      legend: {
-                          display: true,
-                          position: "right"
-                      }
-                  }}
-                />
-            </div>
-        );
-    }
+                }}
+            />
+        </div>
+    );
+}
 
 export default History;
